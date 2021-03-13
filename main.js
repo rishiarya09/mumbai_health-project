f744fb376927dde7c1da5f77b6567fde08001959
rishiarya <rishairya09@gmail.com>
2021-03-12 22:12:49 -0400

    index page and about us page is done and op struct mobile is not done

---
+++ main.js
@@ -0,0 +1,15 @@
+// nav bar toggle functionality
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('slider');
+let mainmenu = document.getElementById('main');
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     navBarToggle.addEventListener('click', function () {
+//         mainNav.classList.toggle('active');
+//     });
+// });
+function navbarToggle(){
+    mainNav.classList.toggle('closed');
+    // mainmenu.classList.toggle('opened');
+}
+// navv bar toggle functionality ends here
