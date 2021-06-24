22de9d382a9e3c552537671c28c1d3405536cc30
rishiarya09@gmail.com <rishiarya09@gmail.com>
2021-06-24 13:16:06 -0300

    updated for header and footer for second version

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+let navToggle = document.querySelector(".nav__toggle");
+let navWrapper = document.querySelector(".nav__wrapper");
+
+navToggle.addEventListener("click", function () {
+  if (navWrapper.classList.contains("active")) {
+    // this.setAttribute("aria-expanded", "false");
+    // this.setAttribute("aria-label", "menu");
+    navWrapper.classList.remove("active");
+  } else {
+    navWrapper.classList.add("active");
+    // this.setAttribute("aria-label", "close menu");
+    // this.setAttribute("aria-expanded", "true");
+  }
+});
+
+
+
+
 // nav bar toggle functionality
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('slider');
