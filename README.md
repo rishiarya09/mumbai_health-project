d153d4bd1cd9325ca466a69906db498ceab05367
bhavi140201 <68502679+bhavi140201@users.noreply.github.com>
2021-12-04 01:55:48 +0530

    Final Updated Files Commit on 4/12/2021

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mumbai-Health-Project
+Website for Mumbai Health Project MHP
---
+++ taximeter_script.js
@@ -0,0 +1,34 @@
+$(window).scroll(testScroll);
+var viewed = false;
+
+function isScrolledIntoView(elem) {
+  var docViewTop = $(window).scrollTop();
+  var docViewBottom = docViewTop + $(window).height();
+
+  var elemTop = $(elem).offset().top;
+  var elemBottom = elemTop + $(elem).height();
+
+  return elemBottom <= docViewBottom && elemTop >= docViewTop;
+}
+
+function testScroll() {
+  if (isScrolledIntoView($(".numbers")) && !viewed) {
+    viewed = true;
+    $(".value").each(function () {
+      $(this)
+        .prop("Counter", 20000)
+        .animate(
+          {
+            Counter: $(this).text(),
+          },
+          {
+            duration: 10000,
+            easing: "swing",
+            step: function (now) {
+              $(this).text(Math.ceil(now));
+            },
+          }
+        );
+    });
+  }
+}
